08cc24bd7cde6236db78311687542e47746bde6b
Luis Rojas <luisrohu91@gmail.com>
2021-02-03 17:25:09 -0700

    Update ReadMe/add constructor to email.

--- practice_project/src/emailApp/Email.java
+++ practice_project/src/emailApp/Email.java
@@ -1,5 +1,26 @@
 package emailApp;
 
 public class Email {
-
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String altEmail;
+	
+	// constructor takes in first and last name 
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email Created: " + this.firstName + " "+ this.lastName); 
+	}
+	// ask for department
+	
+	// generate password
+	
+	// set mailbox capacity
+	
+	// set alt email
+	
+	// change password
 }
--- practice_project/src/emailApp/EmailApp.java
+++ practice_project/src/emailApp/EmailApp.java
@@ -3,8 +3,8 @@ package emailApp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// use constructor
+		Email em1 = new Email("John", "Smith");
 	}
 
 }
