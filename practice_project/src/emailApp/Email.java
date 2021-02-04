f431f4a09946d620bed3035463ca848494cae2e4
Luis Rojas <luisrohu91@gmail.com>
2021-02-03 17:34:12 -0700

    add method to ask for department

--- practice_project/src/emailApp/Email.java
+++ practice_project/src/emailApp/Email.java
@@ -1,4 +1,6 @@
 package emailApp;
+import java.util.Scanner;
+
 
 public class Email {
 	private String firstName;
@@ -12,10 +14,23 @@ public class Email {
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		System.out.println("Email Created: " + this.firstName + " "+ this.lastName); 
+		System.out.println("Email Created: " + this.firstName + " "+ this.lastName);
+		
+		// method to ask for department
+		this.department = setDepartment();
+		System.out.println("Deparment: " + this.department);
 	}
-	// ask for department
-	
+	// ask for department and return a department
+	private String setDepartment() {
+		System.out.print("Enter the department: \n 1 for Sales \n 2 for Accounting \n 0 for None");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		
+		if (depChoice == 1 ) {return "sales";}
+		else if (depChoice == 2) {return "accounting";}
+		else { return "";}
+		
+	}
 	// generate password
 	
 	// set mailbox capacity
